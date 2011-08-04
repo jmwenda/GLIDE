c84552032456e49022ed41b92435b25692cedd63
Jude Mwenda <judemwenda@gmail.com>
2011-08-04 19:07:01 +0300

    Closes gh-6. Unicode sorted and verbose plural names sorted

--- GLIDE/GLIDEAPP/admin.py
+++ GLIDE/GLIDEAPP/admin.py
@@ -2,6 +2,9 @@ from GLIDEAPP.models import event,country,GLIDE
 from django.contrib import admin
 from django.contrib.gis import admin
 #sort out tht inlines later
-admin.site.register(event)
+class eventAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(event,eventAdmin)
 admin.site.register(country)
 admin.site.register(GLIDE,admin.GeoModelAdmin)
--- GLIDE/GLIDEAPP/models.py
+++ GLIDE/GLIDEAPP/models.py
@@ -4,8 +4,14 @@ from django.contrib.gis.db import models
 # Create your models here.
 class event(models.Model):
 	event = models.CharField("Event",max_length=120)
+        def __unicode__(self):
+		return self.event
 class country(models.Model):
 	country = models.CharField("Event",max_length=120)
+        def __unicode__(self):
+		return self.country
+        class Meta:
+		verbose_name_plural = "Countries"
 class GLIDE(models.Model):
 	glidenumber = models.CharField("Glide Number",max_length=120)
 	event = models.ForeignKey(event,null=False)
@@ -14,4 +20,8 @@ class GLIDE(models.Model):
         #we now add the GIS juice over here
         polygeom = models.MultiPolygonField()
         objects = models.GeoManager()
+        def __unicode__(self):
+		return self.glidenumber
+        class Meta:
+		verbose_name_plural = "GLIDE Occurences"
         
