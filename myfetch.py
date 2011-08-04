ba17080bd5b4bcf5033d43d1661a8198773b8071
Jude Mwenda <judemwenda@gmail.com>
2011-08-05 01:08:22 +0300

    the myfetch.py on running retuns recored for the first 25 records and loop for subsequent pages, sort insertion to database,and addition and django custom management command

--- myfetch.py
+++ myfetch.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 import urllib,urllib2
 from BeautifulSoup import BeautifulSoup 
+import codecs
 url = 'http://www.glidenumber.net/glide/public/search/search.jsp'
-parameters = {'maxhits' : '25','X_Resolution' : '1440','sortby': '0','process':'0','posted':'0','nStart':'25', 'level0': '*','level1': '*','ftoption': '&','events':'*'} 
+parameters = {'maxhits' : '25','X_Resolution' : '1440','sortby': '0','process':'0','posted':'0','nStart':'0', 'level0': '*','level1': '*','ftoption': '&','events':'*'} 
 data = urllib.urlencode(parameters)    # Use urllib to encode the parameters
 request = urllib2.Request(url, data)
 response = urllib2.urlopen(request)    # This request is sent in HTTP POST
 page = response.read()
 soup = BeautifulSoup(page)
-mydata = soup.findAll("td", {"class" : "basefontSmall"})
+table = soup.find( "table", {"border":"1", "width":"100%", "cellspacing":"1", "cellpadding":"1"} )
+records=[]
+for row in table.findAll('tr'):
+	col = row.findAll("td", {"class" : "basefontSmall"})
+        for td in col:
+		td.find(text=True)
 
-#print mydata
-#links = soup.findAll("a", {"class" : "bluelinks"})
-counter = 0
-while counter < 103 :
-     start = mydata[counter].string
-     print start
-     counter = counter + 1
-
-#print page
+                
