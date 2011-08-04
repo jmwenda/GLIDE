a2d3086d8686e8217ecf9625e034470b99b9cfcc
Jude Mwenda <judemwenda@gmail.com>
2011-08-04 14:33:37 +0300

    added the models to the admin, need to sort out the unicode though

---
+++ GLIDE/GLIDEAPP/admin.py
@@ -0,0 +1,6 @@
+from GLIDEAPP.models import event,country,GLIDE
+from django.contrib import admin
+#sort out tht inlines later
+admin.site.register(event)
+admin.site.register(country)
+admin.site.register(GLIDE)
